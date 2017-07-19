66cafb3953b77d30107c1ef0389f6df9e4ff6fad
diegothuran <diego.thuran@gmail.com>
2017-07-19 11:07:26 -0300

    tudo novo

--- Rambo/urls.py
+++ Rambo/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
     url(r'^admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name='index.html'),
+        name='home'),
     url(r'^sentiment/', include('textClassification.urls')),
 
 ]
