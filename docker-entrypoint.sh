4c4f9f050e91598b308e66c71e2472c26046a944
diegothuran <diego.thuran@gmail.com>
2017-10-18 15:26:21 -0300

    new relic configuration

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,4 @@
 
 python /manage.py migrate                  # Apply database migrations
 python /manage.py collectstatic --noinput  # Collect static files
-python /manage.py runserver 0.0.0.0:8000   # Run server
+NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program python /manage.py runserver 0.0.0.0:8000   # Run server
